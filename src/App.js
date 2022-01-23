501bf0c8461a366fcd552b56a5dffc652402e0d3
markdenardo <mark.denardo@gmail.com>
2022-01-23 17:33:11 -0500

    new api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [joke, setJoke] = useState('');
+  const [joke, setJoke] = useState("");
 
   const hook = () => {
     axios
-      .get('https://official-joke-api.appspot.com/jokes/random')
-      .then(response => {
-        console.log(response)
-        setJoke(response.data)
-      })
-  }
+      .get("https://cat-fact.herokuapp.com/facts/random")
+      .then((response) => {
+        setJoke(response.data.text);
+      });
+  };
 
-  useEffect(hook,[])
+  useEffect(hook, []);
 
-  console.log(joke.setup)
+  console.log(joke.setup);
   return (
     <div className="App">
-     <ol> {joke.setup}
-      {joke.punchline}
-      <button onClick={hook}>new joke?</button></ol>
+      <ol>
+        {" "}
+        {joke}
+        <button onClick={hook}>new joke?</button>
+      </ol>
     </div>
   );
 }
