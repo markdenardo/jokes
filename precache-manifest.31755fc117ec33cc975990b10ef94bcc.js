ec30c37c0ec9fbfd21974d70198f55abe9c4dd2d
markdenardo <mark.denardo@gmail.com>
2022-01-23 18:25:18 -0500

    Updates

---
+++ precache-manifest.31755fc117ec33cc975990b10ef94bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6dc91c71f2e4b32da018350ea80cd4",
+    "url": "/jokes/index.html"
+  },
+  {
+    "revision": "f61d800dd72c9b3ed1cd",
+    "url": "/jokes/static/css/main.55ea164d.chunk.css"
+  },
+  {
+    "revision": "e6dcfacc8bbc0a5fff89",
+    "url": "/jokes/static/js/2.6724e6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jokes/static/js/2.6724e6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61d800dd72c9b3ed1cd",
+    "url": "/jokes/static/js/main.d1e70b2c.chunk.js"
+  },
+  {
+    "revision": "e7def931793170d48ceb",
+    "url": "/jokes/static/js/runtime-main.909fc395.js"
+  }
+]);
